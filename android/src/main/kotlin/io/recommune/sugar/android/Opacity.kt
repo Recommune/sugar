2cc805108ec8f56e19a4b01a37a95c41afeda0f2
Quentin LITZLER <qlitzler@gmail.com>
2018-07-17 16:24:02 +0200

    [Android] Opacity and OpacityMaterial

---
+++ android/src/main/kotlin/io/recommune/sugar/android/Opacity.kt
@@ -0,0 +1,27 @@
+package io.recommune.sugar.android
+
+
+object Opacity {
+
+    const val p100 = 0xFF
+    const val p95 = 0xF2
+    const val p90 = 0xE6
+    const val p85 = 0xD9
+    const val p80 = 0xCC
+    const val p75 = 0xBF
+    const val p70 = 0xB3
+    const val p65 = 0xA6
+    const val p60 = 0x99
+    const val p55 = 0x8C
+    const val p50 = 0x80
+    const val p45 = 0x73
+    const val p40 = 0x66
+    const val p35 = 0x59
+    const val p30 = 0x4D
+    const val p25 = 0x40
+    const val p20 = 0x33
+    const val p15 = 0x26
+    const val p10 = 0x1A
+    const val p5 = 0x0D
+    const val p0 = 0x00
+}
---
+++ android/src/main/kotlin/io/recommune/sugar/android/OpacityMaterial.kt
@@ -0,0 +1,13 @@
+package io.recommune.sugar.android
+
+
+object OpacityMaterial {
+
+    const val p100 = 0xFF
+    const val p87 = 0xDE
+    const val p70 = 0xB3
+    const val p54 = 0x8A
+    const val p50 = 0x80
+    const val p38 = 0x61
+    const val p12 = 0x1F
+}
