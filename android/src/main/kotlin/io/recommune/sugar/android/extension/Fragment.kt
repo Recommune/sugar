bb20236cb040a4258f6193d24de7583f93c7e623
Quentin LITZLER <qlitzler@gmail.com>
2018-10-08 14:52:25 +0200

    [Android] Require activity extension

--- android/src/main/kotlin/io/recommune/sugar/android/extension/Fragment.kt
+++ android/src/main/kotlin/io/recommune/sugar/android/extension/Fragment.kt
@@ -6,7 +6,8 @@ import android.support.v7.app.AppCompatActivity
 import android.view.inputmethod.InputMethodManager
 
 
-val Fragment.appcompatActivity get() = requireActivity() as AppCompatActivity
-val Fragment.supportActionBar get() = appcompatActivity.supportActionBar!!
+val Fragment.fragmentActivity get() = requireActivity()
+val Fragment.appCompatActivity get() = requireActivity() as AppCompatActivity
+val Fragment.supportActionBar get() = appCompatActivity.supportActionBar!!
 val Fragment.getContext get() = requireContext()
-val Fragment.inputMethodManager get() = appcompatActivity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+val Fragment.inputMethodManager get() = appCompatActivity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
